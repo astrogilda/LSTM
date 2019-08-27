d21453348ffeeee0989abbc6cbb7333f43d5f1e1
Yuan-Sen Ting <yting@yuan-sens-mbp-3.wireless.ias.edu>
2019-08-27 09:39:13 -0400

    Update

--- real_nvp_deconvolved_distribution.py
+++ real_nvp_deconvolved_distribution.py
@@ -70,7 +70,7 @@ class RealNVP(nn.Module):
 # In [3]:
 # define network
 device = torch.device("cuda")
-num_neurons = 100
+num_neurons = 300
 
 # input dimension
 dim_in = y_tr.shape[-1]
@@ -83,7 +83,7 @@ nett = lambda: nn.Sequential(nn.Linear(dim_in, num_neurons), nn.LeakyReLU(),\
                              nn.Linear(num_neurons, dim_in)).cuda()
 
 # define mask
-num_layers = 5
+num_layers = 10
 masks = []
 for i in range(num_layers):
     mask_layer = np.random.randint(2,size=(dim_in))
